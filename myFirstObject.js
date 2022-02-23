81dcd68a822d0b3a4d5853a20bb4bc19fa936b81
Andrejjj1 <99968251+Andrejjj1@users.noreply.github.com>
2022-02-24 00:10:47 +0300

    Update myFirstObject.js
    Add method console.log

--- myFirstObject.js
+++ myFirstObject.js
@@ -8,3 +8,8 @@ color : 'red',
 maxSpeed : '210'
 }
 car1.color = car2.color
+console.log(typeof car1)
+console.log(typeof car2)
+console.log(typeof car1.color)
+console.log(car1.color)
+console.log(car2.color)
